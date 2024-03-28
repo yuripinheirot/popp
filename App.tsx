80a09e1b2cb587f88fdc366c1de59fa2741c64d2
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-03-28 12:31:34 -0400

    feat: add initial view searchplace

--- App.tsx
+++ App.tsx
@@ -2,14 +2,13 @@ import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import { Button } from '@rneui/themed'
 import { Contexts } from './src/contexts'
+import { SearchPlaceView } from './src/views/SearchPlace'
 
 export default function App() {
   return (
     <Contexts>
       <SafeAreaView className='flex-1 items-center justify-center '>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style='auto' />
-        <Button color='secondary'>asdads</Button>
+        <SearchPlaceView />
       </SafeAreaView>
     </Contexts>
   )
---
+++ src/views/SearchPlace/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native'
+
+export const SearchPlaceView = () => {
+  return <Text>SearchPlaceView</Text>
+}
