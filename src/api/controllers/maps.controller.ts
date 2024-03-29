266f46c10b5bad7939fde345a05b770ae8e0fd93
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-03-28 21:10:03 -0400

    chore: add controllers settings

---
+++ src/api/controllers/maps.controller.ts
@@ -0,0 +1,14 @@
+import { http } from '../http'
+import { PlacesNearbyResponse } from '@googlemaps/google-maps-services-js'
+import { SearchNearbyPlacesQueryType } from '../protocols/searchNearbyPlacesQuery.type'
+
+export const mapsController = {
+  searchNearbyPlaces: async (query: SearchNearbyPlacesQueryType) => {
+    const { data } = await http.get<PlacesNearbyResponse>(
+      '/maps/search/nearby',
+      { params: query }
+    )
+
+    return data
+  },
+}
---
+++ src/api/protocols/searchNearbyPlacesQuery.type.ts
@@ -0,0 +1,7 @@
+export type SearchNearbyPlacesQueryType = {
+  lat: number
+  lng: number
+  radius: number
+  type: string
+  keyword: string
+}
